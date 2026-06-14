ee86e332b4656836ff716237b12c3533f310705c
xaviercariza <xcariza@gmail.com>
2026-06-14 09:05:10 +0200

    refactor(core): simplify contract types

--- packages/core/src/contract.ts
+++ packages/core/src/contract.ts
@@ -1,25 +1,40 @@
 import type { z } from 'zod'
+import type { InferSchema } from './types'
 
 type ActionOptions = {
   validate?: boolean
 }
+
 type ListenerOptions = {
   validate?: boolean
 }
 
-type TBaseAction = {
-  input: z.ZodSchema
+type TBaseAction<TInput extends z.ZodTypeAny = z.ZodTypeAny> = {
+  type: 'action'
+  input: TInput
+  options?: ActionOptions
+  response?: undefined
+}
+
+type TActionWithAck<
+  TInput extends z.ZodTypeAny = z.ZodTypeAny,
+  TOutput extends z.ZodTypeAny = z.ZodTypeAny,
+> = {
+  type: 'action'
+  input: TInput
+  response: TOutput
   options?: ActionOptions
 }
 
-type TActionWithAck = TBaseAction & {
-  response: z.ZodSchema
-}
 type IoAction = TBaseAction | TActionWithAck
-type IoListener = ListenerOptions & { data: z.ZodSchema | Zod.ZodVoid }
+type IoListener<TData extends z.ZodTypeAny = z.ZodTypeAny> = ListenerOptions & {
+  type: 'listener'
+  data: TData
+}
+
 type ContractType = 'action' | 'listener'
-type ContractAction = { type: 'action' } & IoAction
-type ContractListener = { type: 'listener' } & IoListener
+type ContractAction = IoAction
+type ContractListener = IoListener
 type ContractRouterType = {
   [key: string]: ContractRouterType | ContractAction | ContractListener
 }
@@ -31,66 +46,68 @@ const defineContract = <TContractRouter extends ContractRouterType>(
 const isContractRouter = (
   contract: ContractRouterType | ContractAction | ContractListener
 ): contract is ContractRouterType => {
-  return contract.type !== 'action' && contract.type !== 'listener'
+  const type = (contract as { type?: unknown }).type
+  return type !== 'action' && type !== 'listener'
 }
 
 const isContractListener = (
   contract: ContractRouterType | ContractAction | ContractListener
 ): contract is ContractListener => {
-  return contract.type === 'listener'
+  return (contract as { type?: unknown }).type === 'listener'
 }
 
 const isContractAction = (
   contract: ContractRouterType | ContractAction | ContractListener
 ): contract is ContractAction => {
-  return contract.type === 'action'
+  return (contract as { type?: unknown }).type === 'action'
 }
 
-const isActionWithAck = (action: TActionWithAck | TBaseAction): action is TActionWithAck => {
-  return (action as TActionWithAck).response !== undefined
+const isActionWithAck = (action: ContractAction): action is TActionWithAck => {
+  return 'response' in action && action.response !== undefined
 }
+
+type InferActionInput<Action> = Action extends { input: infer Input extends z.ZodTypeAny }
+  ? InferSchema<Input>
+  : never
+
+type InferActionOutput<Action> = Action extends { response: infer Output extends z.ZodTypeAny }
+  ? InferSchema<Output>
+  : void
+
+type InferListenerData<Listener> = Listener extends { data: infer Data extends z.ZodTypeAny }
+  ? InferSchema<Data>
+  : never
 
 type AnyContractActions = ContractActions<any>
 type ContractActions<Contract extends ContractRouterType> = {
-  [K in keyof Contract as Contract[K] extends ContractListener
-    ? never
-    : K]: Contract[K] extends ContractRouterType
-    ? ContractActions<Contract[K]>
-    : Contract[K] extends ContractAction
-      ? Contract[K]
+  [K in keyof Contract as Contract[K] extends ContractListener ? never : K]: Contract[K] extends ContractAction
+    ? Contract[K]
+    : Contract[K] extends ContractRouterType
+      ? ContractActions<Contract[K]>
       : never
 }
 
 type AnyContractListeners = ContractListeners<any>
 type ContractListeners<Contract extends ContractRouterType> = {
-  [K in keyof Contract as Contract[K] extends ContractAction
-    ? never
-    : K]: Contract[K] extends ContractRouterType
-    ? ContractListeners<Contract[K]>
-    : Contract[K] extends ContractListener
-      ? Contract[K]
+  [K in keyof Contract as Contract[K] extends ContractAction ? never : K]: Contract[K] extends ContractListener
+    ? Contract[K]
+    : Contract[K] extends ContractRouterType
+      ? ContractListeners<Contract[K]>
       : never
 }
 
-type FilterPathsByType<
+type ContractPaths<
   Contract extends ContractRouterType,
-  Path extends string,
   Type extends ContractType,
-> =
-  ValueAtPath<Contract, Path> extends ContractAction | ContractListener
-    ? ValueAtPath<Contract, Path>['type'] extends Type
-      ? Path
+> = {
+  [K in keyof Contract & string]: Contract[K] extends { type: Type }
+    ? K
+    : Contract[K] extends ContractRouterType
+      ? `${K}.${ContractPaths<Contract[K], Type>}`
       : never
-    : never
-type Leaves<T> = T extends object
-  ? {
-      [K in keyof T]: T[K] extends IoAction | IoListener
-        ? Exclude<K, symbol>
-        : `${Exclude<K, symbol>}${Leaves<T[K]> extends never ? '' : `.${Leaves<T[K]>}`}`
-    }[keyof T]
-  : never
+}[keyof Contract & string]
 
-type ValueAtPath<T, P> = P extends `${infer Key}.${infer Rest}`
+type ValueAtPath<T, P extends string> = P extends `${infer Key}.${infer Rest}`
   ? Key extends keyof T
     ? ValueAtPath<T[Key], Rest>
     : never
@@ -98,23 +115,36 @@ type ValueAtPath<T, P> = P extends `${infer Key}.${infer Rest}`
     ? T[P]
     : never
 
-type GetKeys<Obj extends object> = keyof Obj
+type EmitEventToFunction<Contract extends ContractRouterType> = <
+  ListenerKey extends ContractPaths<Contract, 'listener'>,
+>(
+  listenerKey: ListenerKey,
+  socketId: string,
+  data: InferListenerData<ValueAtPath<Contract, ListenerKey>>
+) => void
 
-type ContractPaths<Contract extends ContractRouterType, Type extends ContractType> = GetKeys<{
-  [Path in Leaves<Contract> as FilterPathsByType<Contract, Path, Type>]: Path
-}>
+type AnyEmitEventToFunction = EmitEventToFunction<any>
 
-export { defineContract, isContractAction, isContractListener, isContractRouter, isActionWithAck }
+export { defineContract, isActionWithAck, isContractAction, isContractListener, isContractRouter }
 export type {
-  TBaseAction,
-  TActionWithAck,
+  ActionOptions,
   AnyContractActions,
   AnyContractListeners,
+  AnyEmitEventToFunction,
   ContractAction,
   ContractActions,
   ContractListener,
   ContractListeners,
   ContractPaths,
   ContractRouterType,
+  ContractType,
+  EmitEventToFunction,
+  InferActionInput,
+  InferActionOutput,
+  InferListenerData,
+  IoAction,
+  IoListener,
+  TActionWithAck,
+  TBaseAction,
   ValueAtPath,
 }
