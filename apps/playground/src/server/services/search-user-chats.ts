295a8cb9f8b923ef1119c145636f7ced0cfbeaf9
xaviercariza <xcariza@gmail.com>
2026-06-15 08:14:33 +0200

    refactor playground search users

--- apps/playground/src/server/services/search-user-chats.ts
+++ apps/playground/src/server/services/search-user-chats.ts
@@ -1,32 +1,11 @@
 import type { Response, UserProfile } from '../../types'
-import { prisma } from '../prisma'
+import { searchUsers } from '../store'
 
 const searchUserChats = async (
   search: string,
   userNickname: string
 ): Promise<Response<UserProfile[]>> => {
-  const users = await prisma.user.findMany({
-    where: {
-      AND: [
-        {
-          nickname: {
-            contains: search,
-          },
-        },
-        {
-          nickname: {
-            not: userNickname,
-          },
-        },
-      ],
-    },
-    select: {
-      id: true,
-      nickname: true,
-    },
-  })
-
-  return { success: true, data: users }
+  return { success: true, data: searchUsers(search, userNickname) }
 }
 
 export { searchUserChats }
